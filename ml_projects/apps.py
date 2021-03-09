a2d1477317cd451dbc29e84e8e6b0555aff51d89
Samuel <samtheo1597@gmail.com>
2021-03-09 08:11:54 +0100

    Addition of ML Tooys project to the Sooth Sayer project.

---
+++ ml_projects/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MlProjectsConfig(AppConfig):
+    name = 'ml_projects'
---
+++ ml_projects/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.landing_page, name = 'index'),
+    path('summarizer/', views.web_summarizer, name = 'summarizer'),
+    path('tweet_sentiments/', views.twitter, name='twitter'),
+    path('names/', views.name_classify, name='nigerian_names'),
+]
+
